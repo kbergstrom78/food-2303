d9f35a720099cd09faf6e987b4952bd06a808d23
Kim Bergstrom <kimbergstrom78@gmail.com>
2023-07-21 10:36:25 -0600

    update test data to match US

--- spec/features/foods/index_spec.rb
+++ spec/features/foods/index_spec.rb
@@ -5,7 +5,7 @@ RSpec.describe 'As a user' do
     before :each do
       visit root_path
 
-      fill_in 'q', with: 'apple'
+      fill_in 'q', with: 'sweet potatoes'
       click_button 'Search'
     end
 
@@ -15,7 +15,7 @@ RSpec.describe 'As a user' do
 
     it 'shows total results' do
       expect(page).to have_content('Total results')
-      expect(page).to have_content(26109)
+      expect(page).to have_content(55579)
     end
 
     it 'returns a list of 10 items' do
@@ -24,13 +24,13 @@ RSpec.describe 'As a user' do
 
     it 'shows the foods and their attributes' do
       expect(page).to have_content('GTIN/UPC code:')
-      expect(page).to have_content('867824000001')
+      expect(page).to have_content('8901020020844')
       expect(page).to have_content('Description:')
-      expect(page).to have_content('APPLE')
+      expect(page).to have_content('SWEET POTATOES')
       expect(page).to have_content('Brand Owner:')
-      expect(page).to have_content('TREECRISP 2 GO')
+      expect(page).to have_content('John W. Taylor Packing Co. Inc')
       expect(page).to have_content('Ingredients:')
-      expect(page).to have_content('CRISP APPLE.')
+      expect(page).to have_content('SWEET POTATOES.')
     end
   end
 end
